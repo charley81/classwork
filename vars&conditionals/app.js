4d576c291e38f1b05e90fd7e9c1710d482bed333
ChrisKnowsDev <chrisharley81@gmail.com>
2022-08-10 15:54:33 -0400

    added switch and ternary

--- vars&conditionals/app.js
+++ vars&conditionals/app.js
@@ -1,86 +1,86 @@
-// // Ex. 1
-// const varOne = 5
-// const varTwo = 6
-// const varThree = varOne + varTwo
+// Ex. 1
+const varOne = 5
+const varTwo = 6
+const varThree = varOne + varTwo
 
-// // Ex. 2
-// const varFour = 9
-// const varFive = 12
-// const varSix = 15
-// const multiplyNums = varFour * varFive
-// const divideNums = varTwo / varOne
+// Ex. 2
+const varFour = 9
+const varFive = 12
+const varSix = 15
+const multiplyNums = varFour * varFive
+const divideNums = varTwo / varOne
 
-// // Ex. 3
-// const var1 = 2
-// const var2 = 4
-// const var3 = 6
-// const var4 = 8
-// const var5 = 10
-// const var6 = 12
-// const var7 = 14
-// const var8 = 16
-// const var9 = 18
-// const var10 = 20
-// const firstFive = var1 + var2 + var3 + var4 + var5
-// const secondFive = var6 + var7 + var8 + var9 + var10
-// const firstFiveAverage = firstFive / 5
-// const secondFiveAverage = secondFive / 5
-// const totalSum = firstFive + secondFive / 10
+// Ex. 3
+const var1 = 2
+const var2 = 4
+const var3 = 6
+const var4 = 8
+const var5 = 10
+const var6 = 12
+const var7 = 14
+const var8 = 16
+const var9 = 18
+const var10 = 20
+const firstFive = var1 + var2 + var3 + var4 + var5
+const secondFive = var6 + var7 + var8 + var9 + var10
+const firstFiveAverage = firstFive / 5
+const secondFiveAverage = secondFive / 5
+const totalSum = firstFive + secondFive / 10
 
-// const nums = [var1, var2, var3, var4, var5, var6, var7, var8, var9, var10]
+const nums = [var1, var2, var3, var4, var5, var6, var7, var8, var9, var10]
 
-// function exerciseThreeAverage(numsArr, length) {
-//   let average = 0
-//   for (let i = 0; i < length; i++) {
-//     average += numsArr[i]
-//   }
-//   return average
-// }
+function exerciseThreeAverage(numsArr, length) {
+  let average = 0
+  for (let i = 0; i < length; i++) {
+    average += numsArr[i]
+  }
+  return average
+}
 
-// exerciseThreeAverage(nums)
+exerciseThreeAverage(nums)
 
-// // Ex. 4
-// function findAverage(...nums) {
-//   let average = 0
+// Ex. 4
+function findAverage(...nums) {
+  let average = 0
 
-//   for (let i = 0; i < nums.length; i++) {
-//     average += nums[i]
-//   }
+  for (let i = 0; i < nums.length; i++) {
+    average += nums[i]
+  }
 
-//   return average / nums.length
-// }
+  return average / nums.length
+}
 
-// findAverage(4, 10, 22, -30, 55)
-// findAverage(68, 7, -22, 9, 100)
+findAverage(4, 10, 22, -30, 55)
+findAverage(68, 7, -22, 9, 100)
 
-// let box1 = 'hello'
-// let box2 = 'class'
-// let box3
+let box1 = 'hello'
+let box2 = 'class'
+let box3
 
-// box3 = box1 // hello
-// box1 = box2 // class
-// box2 = box3 // hello
+box3 = box1 // hello
+box1 = box2 // class
+box2 = box3 // hello
 
-// // conditionals
-// // Ex.1
-// let num = 5
+// conditionals
+// Ex.1
+let num = 5
 // if (num >= 0) {
 //   console.log('true')
 // } else {
 //   console.log('false')
 // }
 
-// // Ex.2
-// num = -5
+// Ex.2
+num = -5
 // if (num >= 0) {
 //   console.log('true')
 // } else {
 //   console.log('false')
 // }
 
-// // Ex.3
-// const userUnderAge = 17
-// const userOfAge = 18
+// Ex.3
+const userUnderAge = 17
+const userOfAge = 18
 
 // if (userOfAge >= 18) {
 //   console.log('You have access')
@@ -88,19 +88,19 @@
 //   console.log('Access denied')
 // }
 
-// const checkAccess = age => (age >= 18 ? 'Access Granted' : 'Access Denied')
+const checkAccess = age => (age >= 18 ? 'Access Granted' : 'Access Denied')
 
-// function checkAccess2(age) {
-//   if (age >= 18) {
-//     return 'Access Granted'
-//   }
+function checkAccess2(age) {
+  if (age >= 18) {
+    return 'Access Granted'
+  }
 
-//   return 'Access Denied'
-// }
+  return 'Access Denied'
+}
 
-// // nested conditionals
-// // Ex.1
-// let num1 = 0
+// nested conditionals
+// Ex.1
+let num1 = 0
 
 // if (num >= 0) {
 //   if (num > 100) {
@@ -131,17 +131,55 @@
 // if (grade >= 55) console.log('D')
 // if (grade < 55) console.log('F')
 
-// const num3 = 5
-// console.log(num3 % 2 === 0)
+let global = 'global'
 
-// let global = 'global'
+if (true) {
+  // block scope
+  let blockScoped = 'block'
 
-// if (true) {
-//   // block scope
-//   let blockScoped = 'block'
+  if (true) {
+    // nested block scope
+    const nestedBlockScope = 'nested'
+  }
+}
 
-//   if (true) {
-//     // nested block scope
-//     const nestedBlockScope = 'nested'
-//   }
+let number = 2
+
+// switch (number) {
+//   case 1:
+//   case 2:
+//   case 3:
+//     console.log('a number')
+//     break
+//   case 'beef':
+//     console.log('wrong')
+//     break
+//   case 'poltry':
+//     console.log('correct')
+//     break
+//   default:
+//     console.log('no meat')
+//     break
 // }
+
+// ternary operator
+// Ex. 1
+const letter = 'E'
+
+switch (letter) {
+  case 'A':
+  case 'E':
+  case 'I':
+  case 'O':
+  case 'U':
+    console.log('vowel')
+    break
+  default:
+    console.log('consonant')
+}
+
+// Ex. 2
+const number1 = 0
+const status1 =
+  number1 > 0 ? 'Positive' : number1 === 0 ? 'Number is zero' : 'Negative'
+console.log(status1)
