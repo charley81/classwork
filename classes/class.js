66ac3e0355a00acdd4bc16a2e50a7ee0eae43292
ChrisKnowsDev <chrisharley81@gmail.com>
2022-08-17 15:48:55 -0400

    afternoon JS Class labs

--- classes/class.js
+++ classes/class.js
@@ -1,42 +1,42 @@
-class Vehicle {
-  constructor(make, model, year, vin) {
-    this.make = make
-    this.model = model
-    this.year = year
-    this.vin = vin
-    this.running = false
-    console.log(make, model, year, vin)
-  }
+// class Vehicle {
+//   constructor(make, model, year, vin) {
+//     this.make = make
+//     this.model = model
+//     this.year = year
+//     this.vin = vin
+//     this.running = false
+//     console.log(make, model, year, vin)
+//   }
 
-  goVroom() {
-    console.log(`${this.model} goes..... vroooooommmmm!!!!! vrooooommmmm!!!`)
-  }
+//   goVroom() {
+//     console.log(`${this.model} goes..... vroooooommmmm!!!!! vrooooommmmm!!!`)
+//   }
 
-  start() {
-    this.running = true
-    console.log('running')
-  }
+//   start() {
+//     this.running = true
+//     console.log('running')
+//   }
 
-  stop() {
-    this.running = false
-    console.log(
-      `${this.make} ${this.model} ${
-        this.running ? 'Is running' : 'Is NOT running'
-      }`
-    )
-  }
+//   stop() {
+//     this.running = false
+//     console.log(
+//       `${this.make} ${this.model} ${
+//         this.running ? 'Is running' : 'Is NOT running'
+//       }`
+//     )
+//   }
 
-  toString() {
-    return `Vehicle ${this.vin} is a ${this.make} model ${this.model}`
-  }
-}
+//   toString() {
+//     return `Vehicle ${this.vin} is a ${this.make} model ${this.model}`
+//   }
+// }
 
-const ford = new Vehicle('ford', 'f-150', '2017', 'A001')
-// ford.goVroom()
-// ford.start()
-ford.stop()
+// const ford = new Vehicle('ford', 'f-150', '2017', 'A001')
+// // ford.goVroom()
+// // ford.start()
+// ford.stop()
 
-const honda = new Vehicle('honda', 'civic', '2016', 'A002')
+// const honda = new Vehicle('honda', 'civic', '2016', 'A002')
 
 // ========== BREAK ==========
 
@@ -61,3 +61,40 @@ const honda = new Vehicle('honda', 'civic', '2016', 'A002')
 // let user2 = new User('John')
 // user2.sayHi()
 // user.sayGoodbye()
+
+class Character {
+  constructor(name, age, origin) {
+    this.name = name
+    this.age = age
+    this.origin = origin
+    console.log('inside of Character constructor')
+  }
+
+  greet() {
+    console.log(`${this.name} is saying hello`)
+  }
+
+  eat() {
+    console.log(`${this.name} is eating`)
+  }
+}
+
+class Hobbit extends Character {
+  constructor(name, age, eyes, hair) {
+    super(eyes)
+    console.log('inside of Hobbit constructor')
+  }
+  steal() {
+    console.log("I'm stealing")
+  }
+
+  eat() {
+    console.log(`lets is eating`)
+    super.greet()
+  }
+}
+
+const frodo = new Hobbit('frodo', 99, 'waynes world')
+frodo.eat()
+
+const jimmy = new Character('jimmy', 99, 'jims world', 'purple')
--- classes/lab.js
+++ classes/lab.js
@@ -24,16 +24,16 @@ class Cat {
 }
 
 const sam = new Cat('Sam', 8, 'Male')
-console.log(sam)
-sam.eat()
-sam.birthday()
-sam.takesNap()
+// console.log(sam)
+// sam.eat()
+// sam.birthday()
+// sam.takesNap()
 
 const lucy = new Cat('Lucy', 6, 'Female')
-console.log(lucy)
-lucy.eat()
-lucy.birthday()
-lucy.takesNap()
+// console.log(lucy)
+// lucy.eat()
+// lucy.birthday()
+// lucy.takesNap()
 
 console.log('========== Break ===========')
 
@@ -68,11 +68,13 @@ const jollyRoger = [
   new Pirate('Crazy Pirate', 93, 'Paralizes with his thoughts'),
 ]
 
-jollyRoger.forEach(pirate => {
-  console.log(
-    `Name: ${pirate.name} \n Power: ${pirate.power} \n Secret Weapon: ${pirate.secretWeapon}`
-  )
-})
+// jollyRoger[0].powerUp()
+
+// jollyRoger.forEach(pirate => {
+//   console.log(
+//     `Name: ${pirate.name} \n Power: ${pirate.power} \n Secret Weapon: ${pirate.secretWeapon}`
+//   )
+// })
 
 const blackPearl = [
   new Pirate('Death Pearl', 100, 'Spits pearls of death'),
@@ -80,11 +82,11 @@ const blackPearl = [
   new Pirate('Crazy Pirate', 93, 'Paralizes with his thoughts'),
 ]
 
-blackPearl.forEach(pirate => {
-  console.log(
-    `Name: ${pirate.name} \n Power: ${pirate.power} \n Secret Weapon: ${pirate.secretWeapon}`
-  )
-})
+// blackPearl.forEach(pirate => {
+//   console.log(
+//     `Name: ${pirate.name} \n Power: ${pirate.power} \n Secret Weapon: ${pirate.secretWeapon}`
+//   )
+// })
 
 console.log('========== Break ===========')
 
@@ -100,4 +102,123 @@ for (let i = 0; i < 100; i++) {
   freighter.push(new Car(i))
 }
 
-console.log(freighter)
+// console.log(freighter)
+
+console.log('========== Break ===========')
+
+// ===== Ex. 4 =====
+class Governor {
+  static hasOffice = true
+  static writesLaws = true
+
+  static pressConference() {
+    console.log('Giving press conference')
+  }
+
+  static addressPeople() {
+    console.log('Addressing people in the state')
+  }
+}
+
+class Person {
+  constructor(breath, drink, eat) {
+    Person.breath = breath
+    Person.drink = drink
+    Person.eat = eat
+  }
+
+  static isBreating() {
+    console.log("I'm breating")
+  }
+
+  static isDrinking() {
+    console.log("I'm drinking")
+  }
+
+  static isEating() {
+    console.log("I'm eating")
+  }
+}
+
+class PostalWorker extends Person {
+  constructor(name) {
+    super()
+    this.name = name
+  }
+
+  openMailbox() {
+    return `${this.name} is opening the mailbox`
+  }
+
+  checksName() {
+    return `${this.name} is checking name on mail`
+  }
+
+  deliveringMail() {
+    return `${this.name} is delivering the mail`
+  }
+}
+
+console.log('===== 1st postal worker =====')
+const Arrj = new PostalWorker('Aranjyothi')
+
+console.log(Arrj.openMailbox())
+console.log(Arrj.checksName())
+console.log(Arrj.deliveringMail())
+
+console.log('===== 2nd postal worker =====')
+const Chris = new PostalWorker('Chris')
+console.log(Chris.openMailbox())
+console.log(Chris.checksName())
+console.log(Chris.deliveringMail())
+
+class Chef extends Person {
+  constructor(name, food) {
+    super()
+    this.name = name
+    this.food = food
+  }
+  preparesFood() {
+    return `chef ${this.name} preparing ${this.food}`
+  }
+
+  cooksFood() {
+    return `chef ${this.name} is cooking ${this.food}`
+  }
+
+  presentsFood() {
+    return `chef ${this.name} presents ${this.food}`
+  }
+}
+
+console.log('===== 1st Chef =====')
+const Joshua = new Chef('Joshua', 'pizza')
+console.log(Joshua)
+console.log(Joshua.preparesFood())
+console.log(Joshua.cooksFood())
+console.log(Joshua.presentsFood())
+
+console.log('===== 2nd Chef =====')
+
+const John = new Chef('John', 'chicken curry')
+console.log(John)
+console.log(John.preparesFood())
+console.log(John.cooksFood())
+console.log(John.presentsFood())
+
+console.log('========== Break ===========')
+
+// ===== Ex. 5 =====
+class BankAccount {
+  constructor(ownerName, balance) {
+    this.ownerName = ownerName
+    this.balance = balance
+    this.balance = new Date().getMilliseconds()
+  }
+
+  deposit() {}
+
+  withdraw() {}
+}
+
+console.log()
