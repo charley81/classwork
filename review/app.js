6718be4612a07bd3713588fe08cc2605c58894c7
ChrisKnowsDev <chrisharley81@gmail.com>
2022-08-18 14:25:21 -0400

    section I-a complete

--- review/app.js
+++ review/app.js
@@ -1,4 +1,28 @@
-// ===== SECTION I =====
+// ===== SECTION I Vars & Datatypes =====
+// A
+var val = 'A' //1
+val = 'B' //2
+var val2 = val //3
+/**
+ * 4
+ * Declare: When you declare a function
+ * function() {}
+ *
+ * Assgin: When you assign a value to a varable
+ * const letter = 'A'
+ *
+ * Define: When you define a function???
+ * function() {}
+ *
+ * 5
+ * pseudocoding is when you write out the step that you want to code in human readable text. This helps you figure out the step that you need to take to complete the problem
+ *
+ * 6
+ * 90% thinking about how to solve and 10% solving
+ *
+ */
+
+console.log(val2)
 
 // ===== SECTION II =====
 
