4264e3b76eb3f1e3c6b6f1e7797db0870987b4ed
ChrisKnowsDev <chrisharley81@gmail.com>
2022-08-22 15:04:01 -0400

    wild wild life answered except last question

--- review/app.js
+++ review/app.js
@@ -517,8 +517,25 @@ for (let i = 0; i <= 200; i += 2) {
 
 // ===== fizz buzz =====
 for (let i = 1; i <= 100; i++) {
-  if (i % 15 === 0) console.log(`${i}: FizzBuzz`)
-  if (i % 5 === 0) console.log(`${i}: Buzz`)
-  if (i % 3 === 0) console.log(`${i}: Fizz`)
-  console.log(i)
+  // if (i % 15 === 0) console.log(`${i}: FizzBuzz`)
+  // if (i % 5 === 0) console.log(`${i}: Buzz`)
+  // if (i % 3 === 0) console.log(`${i}: Fizz`)
+  // console.log(i)
 }
+
+// ===== wild wild life =====
+const wolfy = ['Wolfy', 'wolf', 16, 'Yukon Territory']
+const sharky = ['Sharky', 'shark', 20, 'Left Coast']
+const plantee = ['Plantee', 'plant', 5000, 'Mordor']
+const porgee = ['Porgee', 'Porg', 186, 'Ahch-To']
+const dart = ["D'Art", 'Demogorgan Dog', 2, 'Upside Down']
+plantee[2]++
+// console.log(plantee)
+wolfy[3] = 'Gotham City'
+// console.log(wolfy)
+dart.push('Hawkins')
+// console.log(dart)
+for (let i = 0; i < wolfy.length; i++) {
+  wolfy[i] = wolfy[i].replace('wolf', 'Gameboy')
+}
+// console.log(wolfy)
