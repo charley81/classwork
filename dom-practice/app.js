c07598e0321b18c57be513b85cb1261fb0f4c1ff
ChrisKnowsDev <chrisharley81@gmail.com>
2022-08-26 09:45:17 -0400

    currently on 5.3 event listerner labs

--- dom-practice/app.js
+++ dom-practice/app.js
@@ -1,37 +1,3 @@
-// const pEl = document.querySelector('.cool')
-// const title = document.querySelector('#title')
-
-// pEl.innerHTML = 'Changed this text to say this'
-
-// console.log(pEl)
-// console.log(pEl.innerHTML)
-// console.log(pEl.textContent)
-// console.log(pEl.innerText)
-
-// pEl.style.rotate = '-4deg'
-// title.style.marginBottom = '1rem'
-// pEl.style.color = '#4B7BEC'
-
-// // get attribute (name)
-// // set attribute (name, value)
-// // has attribute (name)
-// // classList
-
-// const link = document.querySelector('.link')
-// link.setAttribute('href', 'http://www.google.com')
-// link.classList.add('google-link')
-// // console.log(link.classList.contains('link'))
-
-// const liTags = document.querySelectorAll('.list-item')
-// const liTags2 = document.getElementsByTagName('li')
-// const liTags3 = document.getElementsByClassName('list-item')
-
-// liTags.forEach(item => {
-//   console.log(item)
-//   item.style.color = '#DCDEE3'
-//   // item.style.fontSize = '30px'
-// })
-
 const mainEl = document.querySelector('main')
 const nav = document.querySelector('#top-menu')
 
@@ -47,7 +13,7 @@ nav.style.height = '100%'
 nav.style.backgroundColor = 'var(--top-menu-bg)'
 nav.classList.add('flex-around')
 
-// Task 3.0
+// Task 5.0
 const menuLinks = [
   { text: 'about', href: '/about' },
   {
@@ -100,12 +66,13 @@ subMenuEl.style.position = 'absolute'
 subMenuEl.style.top = '0'
 // 5.0 Done
 // 5.1
-const topMenuLinks = document.querySelectorAll('a')
-const showingSubMenu = false
+const topMenuLinks = nav.querySelectorAll('a')
+
+let showingSubMenu = false
 // 5.2
-// addEventListener
 nav.addEventListener('click', e => {
   e.preventDefault()
   if (e.target.tagName !== 'A') return
-  console.log(e.target.innerText)
+  // 5.3
+  console.log(e.target.textContent)
 })
