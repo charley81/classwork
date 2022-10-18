d6f4a868c7d5a790c538c08792e7fceaeea683c9
ChrisKnowsDev <chrisharley81@gmail.com>
2022-10-17 21:26:05 -0400

    added modal with portal

--- korilla-barbq/src/components/home.jsx
+++ korilla-barbq/src/components/home.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
+import Modal from './modal'
 import receiptsData from '../data/receipts'
 import Nav from './nav'
 import Receipts from './receipts'
 
 const Home = () => {
   const [receipts, setReceipts] = useState(receiptsData)
+  const [showModal, setShowModal] = useState(false)
 
   const handlePaid = id => {
+    setShowModal(true)
     setReceipts(current =>
       current.map(item => {
         if (item.id === id) {
@@ -22,6 +25,16 @@ const Home = () => {
     <div>
       <Nav />
       <Receipts receipts={receipts} onHandlePaid={handlePaid} />
+      {showModal ? (
+        <Modal>
+          <div>
+            <h1>Receipt Removed</h1>
+            <button className="button" onClick={() => setShowModal(false)}>
+              Ok
+            </button>
+          </div>
+        </Modal>
+      ) : null}
     </div>
   )
 }
